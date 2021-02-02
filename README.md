01421f4db993f555d74b8594708a0b8a8d52b8d7
Nicole Sevillano <34521909+s3vi26@users.noreply.github.com>
2021-02-02 12:37:00 -0500

    readme

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # ParsingTest
+This is a simple parsing tool with a test file: `test.ini`
+It uses the stdlib `configparser`
 
-stdlib used: configparser
+# Usage
+
+Run the `parse.py` by using:
+```bash
+python parse.py
+```
+
+It will prompt you with an `input()` question of "Please provide a Key name you want the Value for: "
+
+If the Key name does not exist it will reask the question and provide the key names available for input. 
--- parse.py
+++ parse.py
@@ -1,24 +1,33 @@
 import configparser
-
+from IPython.display import clear_output
+# set up config parser
 config = configparser.ConfigParser()
 config.sections()
-
-with open('test.ini', 'r') as f:
-  config_file = f.read()
-
-
+# custom config for boolean values to be properly returned
+config.BOOLEAN_STATES = {'on': True, 'off': False, 'yes': True, 'true': True}
+# read file with parser
 config.read('test.ini')
-  
-print(config.sections())
+# set variable for the only section
+default_section = config['DEFAULT']
+# set list variable for all keys to reference
+the_keys_list = list(config['DEFAULT'].keys())
 
-if 'DEFAULT' in config:
-  print(config['DEFAULT'])
-  for key in config['DEFAULT']:
-    print(key)
-
-
-while True: 
-  result = input("Please provide a key to receive it's value: ")
-  for key in config['DEFAULT']:
-    if key == result.lower():
-      print(key)
+result = 'WRONG'
+while result not in the_keys_list:
+  result = input("Please provide a Key name you want the Value for: ").lower()
+  # if the provided input isnt in the_keys_list then continue to ask for Key name
+  if result not in the_keys_list:
+    clear_output()
+    print(f"Sorry, that isnt a Key I can read. Please select from the list below: \n {the_keys_list}")
+    continue
+  # check for keys that deliver boolean values
+  if result == 'test_mode' or result == 'debug_mode' or result == 'verbose' or result == 'send_notifications':
+    print(config['DEFAULT'].getboolean(result))
+  # check for keys that deliver int values
+  elif result == 'server_id':
+    print(int(default_section[result]))
+  # check for keys that deliver float values
+  elif result == 'server_load_alarm':
+    print(float(default_section[result]))
+  else:
+    print(default_section[result])
